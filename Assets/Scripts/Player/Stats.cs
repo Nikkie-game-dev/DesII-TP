5287fdf9ad939f62e82fdf411ab4bbf47a7d0383
Nikkie <54285513+Stevendotcom@users.noreply.github.com>
2025-05-26 21:57:12 -0300

    implement a way to recieve damage

--- Assets/Scripts/Player/Stats.cs
+++ Assets/Scripts/Player/Stats.cs
@@ -1,12 +1,25 @@
+using Services;
 using UnityEngine;
 
 namespace Player
 {
     public class Stats : MonoBehaviour
     {
-        public void ReceiveDamage()
+        [SerializeField] private float health;
+        [SerializeField] private float maxHealth;
+
+        private Service _playerData;
+        private void OnEnable()
         {
-            print("Ouch!");
+            _playerData = ServiceProvider.TryAddService("playerData");
+            ServiceProvider.ChangeAccess(_playerData, AccessType.Put, GetType());
+        }
+
+        public void ReceiveDamage(float damage)
+        {
+            health -= damage;
+            ServiceProvider.Put(_playerData, "health", GetType(), health);
+            UI.HudController.OnTakeDamage.Invoke();
         }
 
         public void MakeGod(out float oldHealth)
